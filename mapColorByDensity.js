b1d6efa11211c91510b86159816b5475356c5b50
Duc Tran <minhduc.gem08@gmail.com>
2020-04-02 10:35:35 +0000

    Create map color table and import data from shapefile table but there are problem with async

--- mapColorByDensity.js
+++ mapColorByDensity.js
@@ -1,34 +1,43 @@
 var pgClient = require('./pgConnection.js');
 
-pgClient.query("CREATE TABLE IF NOT EXISTS public.color_map (gid SERIAL PRIMARY KEY, latitude NUMERIC, longitude NUMERIC, time timestamp WITHOUT time zone, geom geometry(POINT,4326));", (err, res) => {
-    if (err) console.log(err);
-    // console.log(res);
-    console.log();
+pgClient.query("CREATE TABLE IF NOT EXISTS public.map_color (id SERIAL PRIMARY KEY, gid INT, gid_0 VARCHAR (80), gid_1 VARCHAR (80), gid_2 VARCHAR (80), gid_3 VARCHAR (80), color VARCHAR (80));", (err, res) => {
+  if (err) console.log(err);
+  // console.log(res);
+  console.log();
 });
 
-// pgClient.query("CREATE TABLE IF NOT EXISTS public.color_map (gid SERIAL PRIMARY KEY, latitude NUMERIC, longitude NUMERIC, time timestamp WITHOUT time zone, geom geometry(POINT,4326));", (err, res) => {
-// if(err) console.log(err);
-// // console.log(res);
-// console.log();
-// });
-
-pgClient.query("select * from vnm_3 where gid_1 = 'VNM.25_1' limit 10;", (err, res) => {
-  if(res) console.log(res);
-  res.rows.forEach(function (hit) {
-    pgClient.query("INSERT INTO map_color (latitude, longitude, time, geom) VALUES("
-      + hit._source.latitude + ", "
-      + hit._source.longitude + ", "
-      + "(to_timestamp('"
-      + hit._source.time
-      + "', 'yyyy-mm-dd hh24:mi:ss')), "
-      + "ST_GeomFromText('POINT("
-      + hit._source.latitude + " "
-      + hit._source.longitude
-      + ")', 4326));", (err, res) => {
-        if(err) console.log(err);
-        // console.log(err, res);
-      });
-    // console.log(hit);
+pgClient.query("select * from vnm_2 where gid_1 = 'VNM.25_1';", (err, res) => {
+  if (res) console.log(res);
+  res.rows.forEach(function (row) {
+    if (row.gid_3) {
+      pgClient.query("INSERT INTO map_color (gid, gid_0, gid_1, gid_2, gid_3, color) VALUES('"
+        + row.gid + "', '"
+        + row.gid_0 + "', '"
+        + row.gid_1 + "', '"
+        + row.gid_2 + "', '"
+        + row.gid_3
+        + "', '#ffffff');",
+        (err, res) => {
+          if (err) console.log(err);
+          // console.log(err, res);
+        });
+      // console.log(row);
+    }
+    else {
+      pgClient.query("INSERT INTO map_color (gid, gid_0, gid_1, gid_2, gid_3, color) VALUES('"
+        + row.gid + "', '"
+        + row.gid_0 + "', '"
+        + row.gid_1 + "', '"
+        + row.gid_2 + "', "
+        + null 
+        + ", '#ffffff');",
+        (err, res) => {
+          if (err) console.log(err);
+          // console.log(err, res);
+        });
+      // console.log(row);
+    }
   });
-  pgClient.end();
-})
+});
+
+// pgClient.end();
