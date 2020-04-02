60ef3f35d2d084bafd047ed22246546ea918b358
Duc Tran <minhduc.gem08@gmail.com>
2020-04-02 07:57:51 +0000

    Push data from ES to PostGIS successfully

--- getData.js
+++ getData.js
@@ -1,21 +1,79 @@
-// var CronJob = require('cron').CronJob;
-// var job = new CronJob('* * * * * *', function() {
-//   console.log('You will see this message every second');
-// }, null, true, 'America/Los_Angeles');
-// job.start();
-
-
-
-
 var esClient = require('./esConnection.js');
 var pgClient = require('./pgConnection.js');
 
-esClient.cluster.health({},function(err,resp,status) {  
-  console.log("-- Client Health --",resp);
+// --- CREATE TABLE IF NOT EXISTS ---
+pgClient.query("CREATE TABLE IF NOT EXISTS public.vehicle2 (gid SERIAL PRIMARY KEY, latitude NUMERIC, longitude NUMERIC, time timestamp WITHOUT time zone, geom geometry(POINT,4326));", (err, res) => {
+  console.log(res);
+  console.log();
+})
+
+esClient.search({
+  index: 'vehicle',
+  body: {
+    query: {
+      range: {
+        time: {
+          time_zone: "+07:00",
+          gte: "now-1d",
+          lte: "now"
+        }
+      }
+    }
+  }
+}, function (error, response, status) {
+  if (error) {
+    console.log("search error: " + error)
+  }
+  else {
+    console.log("--- Response ---");
+    console.log(response);
+    console.log("--- Hits ---");
+    response.hits.hits.forEach(function (hit) {
+      pgClient.query("INSERT INTO vehicle2 (latitude, longitude, time, geom) VALUES("
+        + hit._source.latitude + ", "
+        + hit._source.longitude + ", "
+        + "(to_timestamp('"
+        + hit._source.time
+        + "', 'yyyy-mm-dd hh24:mi:ss')), "
+        + "ST_GeomFromText('POINT("
+        + hit._source.latitude + " "
+        + hit._source.longitude
+        + ")', 4326));", (err, res) => {
+          console.log(err, res)
+        });
+      console.log(hit);
+    })
+  }
 });
 
 
-pgClient.query('SELECT vnm_3.gid, count(vehicle.geom) AS totale FROM vnm_3 LEFT JOIN vehicle ON st_contains(vnm_3.geom, vehicle.geom) GROUP BY vnm_3.gid;', (err, res) => {
-  console.log(err, res)
-  pgClient.end()
-})
+
+
+
+// pgClient.query('SELECT vnm_3.gid, count(vehicle.geom) AS totale FROM vnm_3 LEFT JOIN vehicle ON st_contains(vnm_3.geom, vehicle.geom) GROUP BY vnm_3.gid;', (err, res) => {
+//   console.log(err, res)
+//   pgClient.end()
+// })
+// pgClient.query("SELECT EXISTS (SELECT FROM pg_tables  WHERE  schemaname = 'public' AND tablename  = 'vehicle2');", (err, res) => {
+//   console.log(res)
+//   pgClient.end()
+// })
+
+
+// --- DROP TABLE ---
+// pgClient.query("DROP TABLE IF EXISTS vehicle2;", (err, res) => {
+//   console.log(res)
+//   pgClient.end()
+// })
+
+
+
+
+
+  // var CronJob = require('cron').CronJob;
+  // var job = new CronJob('* * * * * *', function() {
+  //   console.log('You will see this message every second');
+  // }, null, true, 'America/Los_Angeles');
+  // job.start();
+
+
