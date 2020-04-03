519c59e293d5eb4953e5b5c06dc51df59dd23bb9
Duc Tran <minhduc.gem08@gmail.com>
2020-04-03 09:33:01 +0000

    Refactor project

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var mappingColorRouter = require('./routes/mapping-color');
 
 var app = express();
 
@@ -15,6 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/map/mapping-color', mappingColorRouter);
 
 module.exports = app;
--- routes/mapping-color.js
+++ routes/mapping-color.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET mapping color listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json({successObject: 'Response success'});
 });
 
 module.exports = router;
