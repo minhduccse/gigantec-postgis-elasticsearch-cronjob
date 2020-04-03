be3b30c62b002d7784228e9a2b257a2b84a02554
Duc Tran <minhduc.gem08@gmail.com>
2020-04-03 11:22:50 +0000

    Add update color API

---
+++ routes/update-color.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+const pgPool = require('../db/pgConnection')
+
+router.post('/', function (req, res, next) {
+    pgPool.query("UPDATE color_value SET color_type = '"
+        + req.body.color_type + "' WHERE id = " + req.body.id + "; ")
+        .then(result => {
+            if (result.rowCount > 0) res.json({ successObject: "Updated" });
+        })
+        .catch(err => {
+            console.error('Error executing query', err.stack);
+            res.status(500).json({ failObject: 'Internal Server Error' });
+        });
+});
+
+module.exports = router;
