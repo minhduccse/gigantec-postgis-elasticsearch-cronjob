a51a13ffa49f5a92f9593b600aea46e7aaef861e
Duc Tran <minhduc.gem08@gmail.com>
2020-04-03 10:36:15 +0000

    Try handling error

--- routes/mapping-color.js
+++ routes/mapping-color.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const pgPool = require('../db/pgConnection')
 
 /* GET mapping color listing. */
-router.get('/', function(req, res, next) {
-  res.json({successObject: 'Response success'});
+router.get('/', function (req, res, next) {
+  pgPool.query("SELECT * FROM public.color_value").then(result => res.json(result.rows)).catch(err => {
+    console.error('Error executing query', err.stack);
+    res.status(500).json({ failObject: 'Internal Server Error' });
+  });;
 });
 
 module.exports = router;
