fee9332cdf20e3c22f74ff838c9dea33ecca3796
Duc Tran <minhduc.gem08@gmail.com>
2020-04-03 04:32:59 +0000

    Refactor project: refactor drop tables script

--- cronjob/dropTables.js
+++ cronjob/dropTables.js
@@ -1,11 +1,17 @@
-var pgClient = require('./pgConnection.js');
+var pgClient = require('../db/pgConnection.js');
 
 // --- DROP TABLE ---
-pgClient.query("DROP TABLE IF EXISTS vehicle2;", (err, res) => {
-  console.log(res, err);
-});
+async function dropTables(params) {
+  await pgClient.query("DROP TABLE IF EXISTS vehicle;").then(() => console.log("Dropped table vehicle!"))
+    .catch(err => console.error('Error executing query', err.stack));
+  
+  await pgClient.query("DROP TABLE IF EXISTS population_mapping_color;").then(() => console.log("Dropped table population_map_color!"))
+    .catch(err => console.error('Error executing query', err.stack));
+  
+  await pgClient.query("DROP TABLE IF EXISTS income_mapping_color;").then(() => console.log("Dropped table income_map_color!"))
+    .catch(err => console.error('Error executing query', err.stack));
+  
+  await pgClient.end().then(() => console.log('Pool has ended'));
+}
 
-pgClient.query("DROP TABLE IF EXISTS map_color;", (err, res) => {
-  console.log(res, err);
-  pgClient.end();
-});
+dropTables();
