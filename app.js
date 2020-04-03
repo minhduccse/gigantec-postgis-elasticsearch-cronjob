afc743e6afe930b4cf70ebfd0c8b9bac688f4c8a
Duc Tran <minhduc.gem08@gmail.com>
2020-04-03 11:01:20 +0000

    Configure body-parser

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var mappingColorRouter = require('./routes/mapping-color');
+var updateColorRouter = require('./routes/update-color');
 
 var app = express();
 
@@ -13,8 +15,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/map/mapping-color', mappingColorRouter);
+app.use('/map/update-color', updateColorRouter);
 
 module.exports = app;
