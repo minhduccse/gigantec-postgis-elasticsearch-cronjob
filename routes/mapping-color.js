26649a496ecaab7e4b97f968a25ed788efa87390
Duc Tran <minhduc.gem08@gmail.com>
2020-04-06 08:01:47 +0000

    Add lodash library

--- routes/mapping-color.js
+++ routes/mapping-color.js
@@ -1,12 +1,20 @@
 var express = require('express');
+var _ = require('lodash');
 var router = express.Router();
 const pgPool = require('../db/pgConnection')
 
 /* GET mapping color listing. */
 router.get('/', function (req, res, next) {
-  pgPool.query("SELECT * FROM public.color_value").then(result => res.json(result.rows)).catch(err => {
+  pgPool.query("SELECT * FROM public.color_value").then(result => res.status(200).json({
+    code: 200,
+    successObject: _.orderBy(result.rows, ['id']),
+  }))
+  .catch(err => {
     console.error('Error executing query', err.stack);
-    res.status(500).json({ failObject: 'Internal Server Error' });
+    res.status(500).json({
+      code: 500,
+      failObject: 'Internal Server Error',
+    });
   });;
 });
 
