00ced7876b831cddfa17e1cee886e75321ac41d9
Duc Tran <minhduc.gem08@gmail.com>
2020-04-07 10:11:05 +0000

    Update api response format

--- routes/update/population.js
+++ routes/update/population.js
@@ -7,28 +7,35 @@ router.post('/', function (req, res, next) {
     if (!req.body.gid) {
         return res.status(400).json({
             code: 400,
-            errorMessage: 'gid is required',
+            errorMessage: 'gid Is Required',
         });
     } else if (!req.body.population) {
         return res.status(400).json({
             code: 400,
-            errorMessage: 'population is required',
+            errorMessage: 'population Is Required',
         });
     } else if (!req.body.color_value) {
         return res.status(400).json({
             code: 400,
-            errorMessage: 'color_value is required',
+            errorMessage: 'color_value Is Required',
         });
     }
     const { gid, population, color_value } = req.body;
 
     pgPool.query("UPDATE population_info SET population = $1, color_value = $2 WHERE gid = $3;", [population, color_value, gid])
         .then(result => {
-            if (result.rowCount > 0) res.json({ successObject: "Updated" });
+            if (result.rowCount > 0) res.json({
+                code: 200,
+                successObject: "Updated",
+            });
         })
         .catch(err => {
             console.error('Error executing query', err.stack);
-            res.status(500).json({ failObject: 'Internal Server Error' });
+            res.status(500).json({
+                code: 500,
+                errorMessage: 'Error Executing Query',
+                failObject: 'Internal Server Error',
+            });
         });
 });
 
