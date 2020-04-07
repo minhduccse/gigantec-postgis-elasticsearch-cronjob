9f821d3a5d2f70629faf8310a99328351e3d7c9f
Duc Tran <minhduc.gem08@gmail.com>
2020-04-07 10:07:21 +0000

    Update api response format

--- routes/update/population.js
+++ routes/update/population.js
@@ -4,6 +4,22 @@ var router = express.Router();
 const pgPool = require('../../db/pgConnection')
 
 router.post('/', function (req, res, next) {
+    if (!req.body.gid) {
+        return res.status(400).json({
+            code: 400,
+            errorMessage: 'gid is required',
+        });
+    } else if (!req.body.population) {
+        return res.status(400).json({
+            code: 400,
+            errorMessage: 'population is required',
+        });
+    } else if (!req.body.color_value) {
+        return res.status(400).json({
+            code: 400,
+            errorMessage: 'color_value is required',
+        });
+    }
     const { gid, population, color_value } = req.body;
 
     pgPool.query("UPDATE population_info SET population = $1, color_value = $2 WHERE gid = $3;", [population, color_value, gid])
