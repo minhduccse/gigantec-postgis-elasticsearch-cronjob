500dc44eefc448ae469e81a3ffef4e6fafff239b
Duc Tran <minhduc.gem08@gmail.com>
2020-04-07 08:52:12 +0000

    Add update district population info script

--- routes/update/income.js
+++ routes/update/income.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const pgPool = require('../../db/pgConnection')
 
 router.post('/', function (req, res, next) {
-    pgPool.query("UPDATE color_value SET color_type = '"
+    pgPool.query("UPDATE income_info SET color_type = '"
         + req.body.color_type + "', color_value = '"
         + req.body.color_value + "' WHERE id = "
         + req.body.id + "; ")
---
+++ routes/update/population.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+const pgPool = require('../../db/pgConnection')
+
+router.post('/', function (req, res, next) {
+    pgPool.query("UPDATE population_info SET population = '"
+        + req.body.population + "', color_value = '"
+        + req.body.color_value + "' WHERE id = "
+        + req.body.id + "; ")
+        .then(result => {
+            if (result.rowCount > 0) res.json({ successObject: "Updated" });
+        })
+        .catch(err => {
+            console.error('Error executing query', err.stack);
+            res.status(500).json({ failObject: 'Internal Server Error' });
+        });
+});
+
+module.exports = router;
