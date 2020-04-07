7c27658cbaccb5bcfc08f7d730b7b098be413112
Duc Tran <minhduc.gem08@gmail.com>
2020-04-07 10:47:20 +0000

    Update api response format

--- routes/update/income.js
+++ routes/update/income.js
@@ -4,16 +4,39 @@ var router = express.Router();
 const pgPool = require('../../db/pgConnection')
 
 router.post('/', function (req, res, next) {
-    pgPool.query("UPDATE income_info SET color_type = '"
-        + req.body.color_type + "', color_value = '"
-        + req.body.color_value + "' WHERE id = "
-        + req.body.id + "; ")
+    if (!req.body.id) {
+        return res.status(400).json({
+            code: 400,
+            errorMessage: 'id Is Required',
+        });
+    } else if (!req.body.color_type) {
+        return res.status(400).json({
+            code: 400,
+            errorMessage: 'color_type Is Required',
+        });
+    } else if (!req.body.color_value) {
+        return res.status(400).json({
+            code: 400,
+            errorMessage: 'color_value Is Required',
+        });
+    }
+
+    const { id, color_type, color_value } = req.body;
+
+    pgPool.query("UPDATE income_info SET color_type = $1, color_value = $2 WHERE id = $3", [color_type, color_value, id])
         .then(result => {
-            if (result.rowCount > 0) res.json({ successObject: "Updated" });
+            if (result.rowCount > 0) res.json({
+                code: 200,
+                successObject: "Updated",
+            });
         })
         .catch(err => {
             console.error('Error executing query', err.stack);
-            res.status(500).json({ failObject: 'Internal Server Error' });
+            res.status(500).json({
+                code: 500,
+                errorMessage: 'Error Executing Query',
+                failObject: 'Internal Server Error',
+            });
         });
 });
 
